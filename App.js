df7843c85af8dac9ecf11e2a574ad00394555ab9
manelpin <vitor.pinheiro.ferreira@gmail.com>
2020-01-15 15:10:52 +0000

    inicio

--- App.js
+++ App.js
@@ -4,52 +4,28 @@ import * as React from 'react';
 import { Button, View, Text, Image} from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import  people    from './data/dataProvider'
 
-class LogoTitle extends React.Component {
-  render() {
-    return (
-      <Image
-        source={require('./spiro.png')}
-        style={{ width: 30, height: 30 }}
-      />
-    );
-  }
-}
 
 class HomeScreen extends React.Component {
-  static navigationOptions = ({ navigation}) => {
-    return {
-    headerTitle: () => <LogoTitle />,
-    headerRight: () => (
-      <Button
-        onPress={navigation.getParam('increaseCount')}
-        title="+1"
-        color="000"
-      />
-    ),
-  };
-};
+  
+  
+  
+  
 
-  componentDidMount() {
-    this.props.navigation.setParams({ increaseCount: this._increaseCount });
-  }
-
-  state = {
-    count: 0,
-  };
-
-  _increaseCount = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
+  
 
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Details',{itemId: 77, other: "uma cena"})}
-        />
+        <View>
+          {people.map((person)=>(
+          <Button key={person.id}
+          title={person.first_name}
+          onPress={() => this.props.navigation.navigate('Details',{id: person})}
+        />))}
+        
+        </View>
       </View>
     );
   }
@@ -57,39 +33,23 @@ class HomeScreen extends React.Component {
 
 class DetailsScreen extends React.Component {
  
-  static navigationOptions = ({ navigation, navigationOptions }) => {
-    const { params } = navigation.state;
-
-    return {
-      title: params ? params.otherParam : 'A Nested Details Screen',
-      /* These values are used instead of the shared configuration! */
-      headerStyle: {
-        backgroundColor: navigationOptions.headerTintColor,
-      },
-      headerTintColor: navigationOptions.headerStyle.backgroundColor,
-    };
-  };
+  
 
   render() {
     const {navigation} =this.props;
+    const person = navigation.getParam('id')
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Details Screen</Text>
-        <Text>
-          itemId: {JSON.stringify(navigation.getParam('itemId', 'NO-ID'))}
-        </Text>
-        <Text>
-          otherParam:
-          {JSON.stringify(navigation.getParam('other', 'default value'))}
-        </Text>
-        <Button
-          title="Go to Details... again"
-          onPress={() => this.props.navigation.push('Details',{itemId: Math.floor(Math.random() * 100)})}
-        />
-        <Button
-          title="Go to Home"
-          onPress={() => this.props.navigation.navigate('Home')}
-        />
+        
+        <View>
+          {person.files.map((files)=>(
+          <Button key={files.date}
+          title={files.date+" "+files.time}
+          onPress={() => this.props.navigation.navigate('Details',{itemId: person.id})}
+        />))}
+        
+        </View>
         <Button
           title="Go back"
           onPress={() => this.props.navigation.goBack()}
